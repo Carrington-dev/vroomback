dfe75dd43f87a972bc2ff57549b3cf1452549974
Carrington-dev <crn96m@gmail.com>
2024-08-15 10:30:49 +0200

    Settings and wsgi

--- authenticate/vroomweb/wsgi.py
+++ authenticate/vroomweb/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vroomweb.settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vroomweb.settings.local')
 
 application = get_wsgi_application()
