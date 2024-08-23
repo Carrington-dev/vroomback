a6cf83b3bb56520abe9b7dbab7078198d84934e8
Carrington-dev <crn96m@gmail.com>
2024-08-23 13:54:11 +0200

    Added Templates

--- authenticate/vroomweb/settings/base.py
+++ authenticate/vroomweb/settings/base.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'vroomweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["./templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
