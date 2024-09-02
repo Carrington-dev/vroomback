d7534d19b6a09fb1d62748f36c2cb634915f8419
Carrington-dev <crn96m@gmail.com>
2024-09-02 20:44:38 +0200

    Added Filter by Cities

--- cardealer/tradings/filters.py
+++ cardealer/tradings/filters.py
@@ -1,7 +1,7 @@
 from uuid import UUID
 import django_filters
 from django_filters import rest_framework as filters
-from .models import Make, Vehicle
+from .models import City, Make, Vehicle
 
 class VehicleModelFilter(filters.FilterSet):
     class Meta:
@@ -19,6 +19,7 @@ class VehicleModelFilter(filters.FilterSet):
 
 class VehicleFilter(filters.FilterSet):
     brands = filters.CharFilter(method='filter_by_brands')
+    cities = filters.CharFilter(method='filter_by_cities')
 
     class Meta:
         model = Vehicle
@@ -41,4 +42,12 @@ class VehicleFilter(filters.FilterSet):
         # Filter the queryset by this list
         make_list = [ Make.objects.get(id=id) for id in make_list]
         return queryset.filter(make__in=make_list)
+
+    def filter_by_cities(self, queryset, name, value):
+        # Split the incoming string by commas to create a list of authors
+        city_list = list(map(str, value.split(',')))
+        # Filter the queryset by this list
+        city_list = [ City.objects.get(id=id) for id in city_list]
+        # print(city_list)
+        return queryset.filter(city__in=city_list)
 
