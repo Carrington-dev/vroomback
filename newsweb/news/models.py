5266590c5f84197c9df34ae1f720bd867773791f
Carrington-dev <crn96m@gmail.com>
2024-09-05 12:45:27 +0200

    Added Post Model

--- newsweb/news/models.py
+++ newsweb/news/models.py
@@ -1,3 +1,32 @@
+import uuid
 from django.db import models
+from vroomweb import settings
+from django_resized import ResizedImageField
+from django.template.defaultfilters import slugify
+# from news.fields import OrderField
 
-# Create your models here.
+
+STATUS = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+)
+class News(models.Model):
+    id = models.UUIDField( 
+         primary_key = True, 
+         default = uuid.uuid4, 
+         editable = False)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="news")
+    title = models.CharField(max_length=500)
+    short_description = models.TextField()
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=100, choices=STATUS, default="draft")
+    image = ResizedImageField(size=[1024, 768], crop=['middle', 'center'], upload_to='news/thumbnails')
+
+    def __str__(self):
+        return f"{self.title} *{self.id}"
+
+    def __unicode__(self):
+        return f"{self.title} *{self.id}"
