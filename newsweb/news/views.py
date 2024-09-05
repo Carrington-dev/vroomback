b9472f5a136994f22d83910bc8b01d000d1e8ecd
Carrington-dev <crn96m@gmail.com>
2024-09-05 16:54:20 +0200

    Added News Caching

--- newsweb/news/views.py
+++ newsweb/news/views.py
@@ -1,8 +1,18 @@
 from news.models import Post
 from news.mixins import PostListReadMixin
 from news.serializers import PostSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 class PostViewSet(PostListReadMixin):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     model = Post
+
+    @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 15))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
