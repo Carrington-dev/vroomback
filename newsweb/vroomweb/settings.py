1dab43d0b557d42ef2d65221a5d7e96ad0664893
Carrington-dev <crn96m@gmail.com>
2024-09-05 14:28:48 +0200

    Added Grapphelli

--- newsweb/vroomweb/settings.py
+++ newsweb/vroomweb/settings.py
@@ -12,6 +12,7 @@ DEBUG = config("DEBUG",  default=True, cast=bool)
 ALLOWED_HOSTS = [ "*" ]
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
