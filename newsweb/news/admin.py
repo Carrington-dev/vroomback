6a588d82cccf1df8c3a754c62640952626f550cc
Carrington-dev <crn96m@gmail.com>
2024-09-05 16:12:43 +0200

    Added Post ImageInLine

--- newsweb/news/admin.py
+++ newsweb/news/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from news.models import Image, Post
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 0
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [ 'id' ,'author' ,'title' ,  'short_description' ,'content' ,'image']
-
+    inlines = [ ImageInline ]
 
