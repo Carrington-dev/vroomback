d0533d078a734d4bbb97cf4515cfe93d17c59933
Carrington-dev <crn96m@gmail.com>
2024-09-05 12:27:08 +0200

    Added URLs and requirements

--- newsweb/vroomweb/urls.py
+++ newsweb/vroomweb/urls.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.urls import path, include
+from vroomweb import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
+    path("api/v1/", include("security.urls")),
+    path('api/v1/auth/', include('djoser.urls')),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
+
 ]
+
+
+admin.site.site_title = "Vroohmive Inc"
+admin.site.site_header = "Vroohmive Pty Ltd"
+admin.site.index_title = "Vroohmive welcomes you!!!"
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
