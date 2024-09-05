3c365968c2a5b2e2f0de1afc0889f3b64fb76735
Carrington-dev <crn96m@gmail.com>
2024-09-05 16:36:18 +0200

    Added UserSerializer

--- newsweb/news/serializers.py
+++ newsweb/news/serializers.py
@@ -1,6 +1,13 @@
 from news.models import Image, Post
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'first_name', 'last_name', )
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
