23584a27e019800a7d540c1d4982ac23f241a7bf
Carrington-dev <crn96m@gmail.com>
2024-09-05 13:01:27 +0200

    Added Image Serializer on PostSerializer

--- newsweb/news/serializers.py
+++ newsweb/news/serializers.py
@@ -1,7 +1,15 @@
-from news.models import Post
+from news.models import Image, Post
 from rest_framework import serializers
 
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = [ 'id' , 'photo']
+
 class PostSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Post
-        fields = [ 'id' ,'author' ,'title' ,'short_description' ,'content' ,'image']
+        fields = [ 'id' ,'author' ,'title' , 'images', 'short_description' ,'content' ,'image']
