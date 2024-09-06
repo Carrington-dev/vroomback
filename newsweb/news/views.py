6f1e23279a6bd07a18dead1f86c38ea078cc72f4
Carrington-dev <crn96m@gmail.com>
2024-09-06 12:28:01 +0200

    Added Cateogory Viewset

--- newsweb/news/views.py
+++ newsweb/news/views.py
@@ -1,4 +1,4 @@
-from news.models import Post
+from news.models import Category, Post
 from news.mixins import PostListReadMixin
 from news.serializers import PostSerializer
 from django.utils.decorators import method_decorator
@@ -8,6 +8,19 @@ class PostViewSet(PostListReadMixin):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     model = Post
+
+    @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 15))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+class CategoryViewSet(PostListReadMixin):
+    serializer_class = PostSerializer
+    queryset = Category.objects.all()
+    model = Category
 
     @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
     def list(self, request, *args, **kwargs):
