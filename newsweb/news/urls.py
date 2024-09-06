a051beaad9947681c469257db60c90e349d47bbf
Carrington-dev <crn96m@gmail.com>
2024-09-06 12:29:27 +0200

    Added Category API Endpoints

--- newsweb/news/urls.py
+++ newsweb/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from news.views import PostViewSet
+from news.views import CategoryViewSet, PostViewSet
 
 router =  DefaultRouter()
 router.register("posts", PostViewSet, basename="post")
+router.register("categories", CategoryViewSet, basename="categories")
 
 urlpatterns = [
     path('', include(router.urls))
