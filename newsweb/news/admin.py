0f45d957386f52333b3cf52a07cb71f5b42f5803
Carrington-dev <crn96m@gmail.com>
2024-09-06 12:03:50 +0200

    Added Tag to admin

--- newsweb/news/admin.py
+++ newsweb/news/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from news.models import Category, Image, Post
+from news.models import Category, Image, Post, Tag
 from news.actions import duplicate_post, mark_as_draft, mark_as_published
 
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 0
 
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = [ 'id' , 'name']
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
