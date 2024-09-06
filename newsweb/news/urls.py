de1d04b8a1fa45edf4402a80bc376f2872639574
Carrington-dev <crn96m@gmail.com>
2024-09-06 12:34:01 +0200

    Added Tag endpoint

--- newsweb/news/urls.py
+++ newsweb/news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from news.views import CategoryViewSet, PostViewSet
+from news.views import CategoryViewSet, PostViewSet, TagViewSet
 
 router =  DefaultRouter()
 router.register("posts", PostViewSet, basename="post")
 router.register("categories", CategoryViewSet, basename="categories")
+router.register("tags", TagViewSet, basename="tag")
 
 urlpatterns = [
     path('', include(router.urls))
