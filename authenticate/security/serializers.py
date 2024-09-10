7cc16215afa1ef80864718f9ce3e8eb685c09375
Carrington-dev <crn96m@gmail.com>
2024-09-10 12:57:56 +0200

    Added UserSerializer That Updates Password

--- authenticate/security/serializers.py
+++ authenticate/security/serializers.py
@@ -15,4 +15,17 @@ class UserCreateSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'email', 'username', 'first_name', 'last_name',  'password',)
 
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        try:
+            user.set_password(validated_data['password'])
+            user.save()
+        except KeyError:
+            pass
+        return user
