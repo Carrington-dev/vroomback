6f2b15c670fb856222944b1e0c698c665f937917
Carrington-dev <crn96m@gmail.com>
2024-09-20 12:15:17 +0200

    changed username/reset/.. to email/reset/..

--- authenticate/vroomweb/settings/base.py
+++ authenticate/vroomweb/settings/base.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
 
 DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    'USERNAME_RESET_CONFIRM_URL': 'username/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
     'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
