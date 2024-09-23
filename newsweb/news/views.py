3592cea4af1019893fb0295bf5492aa7190eb3bc
Carrington-dev <crn96m@gmail.com>
2024-09-23 10:57:50 +0200

    Added ContactViewset

--- newsweb/news/views.py
+++ newsweb/news/views.py
@@ -1,9 +1,10 @@
-from news.models import Category, Post, Tag
+from news.models import Category, Contact, Post, Tag
 from news.mixins import PostListReadMixin
-from news.serializers import CategorySerializer, PostSerializer, TagSerializer
+from news.serializers import CategorySerializer, ContactSerializer, PostSerializer, TagSerializer
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django_filters import rest_framework as filters
+from rest_framework import viewsets
 
 class PostViewSet(PostListReadMixin):
     serializer_class = PostSerializer
@@ -37,6 +38,19 @@ class TagViewSet(PostListReadMixin):
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
     model = Tag
+
+    @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 15))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+class ContactViewSet(viewsets.ModelViewSet):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
+    model = Contact
 
     @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
     def list(self, request, *args, **kwargs):
