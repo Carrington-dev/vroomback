a8754d4b4672bfaa0ec88801f388989d65716dcb
Carrington-dev <crn96m@gmail.com>
2024-09-23 10:58:49 +0200

    Added ContactViewSet to urls

--- newsweb/news/urls.py
+++ newsweb/news/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from news.views import CategoryViewSet, PostViewSet, TagViewSet
+from news.views import CategoryViewSet, ContactViewSet, PostViewSet, TagViewSet
 
 router =  DefaultRouter()
 router.register("posts", PostViewSet, basename="post")
 router.register("categories", CategoryViewSet, basename="categories")
 router.register("tags", TagViewSet, basename="tag")
+router.register("contacts", ContactViewSet, basename="contact")
 
 urlpatterns = [
     path('', include(router.urls))
